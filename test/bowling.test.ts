0e4eb6962c8e786d17d133bf018f446ab13561b9
Иван Фадеев <fadeevvania@mail.ru>
2023-10-09 05:53:43 +0500

    refactor: Запрет на превышение границ/Бонусные броски

--- test/bowling.test.ts
+++ test/bowling.test.ts
@@ -10,11 +10,10 @@ beforeEach(() => {
 describe('Bowling Score', () => { 
  
   test('Получение броска и результата одного фрейма', () => { 
-    g.roll(3); // первый бросок первого фрейма 
-    g.roll(4); // второй бросок первого фрейма 
+    g.roll(3); 
+    g.roll(4); 
     expect(g.score()).toBe(7); 
   }); 
-   
  
   test('Все броски мимо', () => { 
     rollMany(20, 0) 
@@ -25,24 +24,31 @@ describe('Bowling Score', () => {
     rollMany(20, 1) 
     expect(g.score()).toBe(20) 
   }) 
+
   test('Слишком много бросков', () => { 
     rollMany(21, 1) 
     expect(g.score()).not.toBeGreaterThan(20) 
   })
+  test('Слишком много Кегель', () => { 
+    rollMany(5, 40) 
+    expect(g.score()).not.toBeGreaterThan(0) 
+  })
+
+
   test('Отрицательные броски', () => { 
     g.roll(-3)
     g.roll(-10)
     expect(g.score()).not.toBeLessThan(0) 
   })  
-   
+ 
   test('Если выпадает spare, то бонус к следующему первому броску', () => { 
     rollSpare() 
     g.roll(9) 
     expect(g.score()).toBe(28) 
   }) 
-   
+
   test('Если страйк, то бонус следующего фрейма', () => { 
-    rollStrike() //strike 
+    rollStrike() 
     g.roll(3) 
     g.roll(4) 
     expect(g.score()).toBe(24) 
@@ -58,19 +64,20 @@ describe('Bowling Score', () => {
     rollStrike();  
     g.roll(5); 
     g.roll(3);  
-    expect(g.score()).toBe(18); 
+    expect(g.score()).toBe(18); / Ошибка /
   }); 
    
   test('Если в последнем фрейме спэр, то один бонусный бросок', () => { 
     rollMany(18, 0);  
     rollSpare();  
     g.roll(5);  
-    expect(g.score()).toBe(15); 
+    expect(g.score()).toBe(15); / Ошибка /
   }); 
+  
+  
 
   
- 
- 
+
   function rollStrike(): void{ 
     g.roll(10) 
   } 
